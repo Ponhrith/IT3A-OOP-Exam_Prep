a582b935e46498c59c24cd9ad6532a89d3978441
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-19 23:39:11 +0700

    First Draft

---
+++ Lesson05Ex1/Lesson05Ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson05Ex1
+{
+    class Point
+    {
+        public float fltX;
+        public float fltY;
+        public float fltZ;
+        private string strType;
+
+        public Point (float fltX, float fltY)
+        {
+            this.fltX = fltX;
+            this.fltY = fltY;
+        }
+
+        public Point(float fltX, float fltY, float fltZ) 
+        {
+            this.fltX = fltX;
+            this.fltY = fltY;
+            this.fltZ = fltZ;
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
