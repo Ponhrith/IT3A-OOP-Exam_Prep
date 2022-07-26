64dc903b20719089e9df699927590ddc2a57f6dc
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-26 17:14:49 +0700

    Done

--- Lesson05Ex2/Lesson05Ex2/Program.cs
+++ Lesson05Ex2/Lesson05Ex2/Program.cs
@@ -6,6 +6,42 @@ using System.Threading.Tasks;
 
 namespace Lesson05Ex2
 {
+    class Time
+    {
+        public int intHour;
+        public int intMin;
+        public int intSec;
+
+        public Time()
+        {
+
+        }
+
+        public Time(int intHour, int intMin, int intSec)
+        {
+            this.intHour = intHour;
+            this.intMin = intMin;
+            this.intSec = intSec;
+        }
+
+        public string GetTime()
+        {
+            string strReturn = (intHour.ToString() + " hours " + intMin.ToString() + " minutes " + intSec.ToString() + " seconds");
+            return strReturn;
+        }
+
+        public float GetTime(string strType)
+        {
+            if (strType == "sec")
+            {
+                intSec = intHour * 3600 + intMin * 60 + intSec;
+            }
+            else if (strType == "min")
+            {
+                intMin = intHour * 60f + intMin + intSec / 60f;
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
