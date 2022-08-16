24190d5b21ef07d216d99324347218641960c3bb
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-16 17:00:31 +0700

    Done

---
+++ Try/Try/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try
+{
+    class GrandFather
+    {
+        private int GrandFatherPrivate = 1;
+        protected int GrandFatherProtected = 2;
+        public int GrandFatherPublic = 3;
+    }
+
+    class Father : GrandFather
+    {
+        private int FatherPrivate = 4;
+        protected int FatherProtected = 5;
+        public int FatherPublic = 6;
+
+        public virtual string ShowInfo()
+        {
+            GrandFatherProtected = 20; //can access protected
+            return FatherPrivate.ToString();
+        }
+    }
+
+    class Son : Father 
+    {
+        private int SonPrivate = 7;
+        protected int SpnProtected = 8;
+        public int SonPublic = 9;
+
+        public override string ShowInfo()
+        {
+            return SonPrivate.ToString();
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Son me = new Son();
+        }
+    }
+}
